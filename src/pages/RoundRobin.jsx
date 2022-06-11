035a89a46ba0e6d65f1f857e9f19eac486f70abd
Marcos Vinicius <mvmarcos74@gmail.com>
2022-06-11 09:36:44 -0300

    update

--- src/pages/RoundRobin.jsx
+++ src/pages/RoundRobin.jsx
@@ -46,10 +46,10 @@ class CircularQueue {
         }
     }
 
-    enqueue(item, id) {
+    enqueue(item, id, pri) {
         if (!this.isFull) {
             this.#tail = (this.#tail + 1) % this.#capacity;
-            this.#list[this.#tail] = { time: item, identificador: id };
+            this.#list[this.#tail] = { time: item, identificador: id, priority: pri };
             this.#size += 1;
 
             if (this.#head === -1) {
@@ -90,7 +90,7 @@ class CircularQueue {
 
 
 const cq = new CircularQueue(5);
-
+const clone = [];
 let listaProntos = [];
 let listaExe = [];
 const spring = {
@@ -107,9 +107,36 @@ export default function RR() {
     const [contador, setContador] = useState(0);
     const [quantum, setQuantum] = useState(300)
     const [executanto, setExecutando] = useState(-1);
+    const [executanto2, setExecutando2] = useState(-1);
     const [contadorProntos, setContadorProntos] = useState(0);
+    const [contadorID, setContadorID] = useState(0);
     const [isOn, setIsOn] = useState(false);
-    
+    const [isOn2, setIsOn2] = useState(false);
+    const [prioridade, setPrioridade] = useState(3);
+console.log(executanto2)
+    function priority1() {
+        setPrioridade(1);
+
+    }
+    function priority2() {
+        setPrioridade(2);
+
+    }
+    function priority3() {
+        setPrioridade(3);
+
+    }
+    function toggleSort() {
+        cq.list.sort(compareP);
+        setIsOn2(!isOn2);
+    }
+    function compareP(a, b) {
+        if (a.priority < b.priority)
+            return -1;
+        if (a.priority > b.priority)
+            return 1;
+        return 0;
+    }
     function compare(a, b) {
         if (a.time < b.time)
             return -1;
@@ -134,13 +161,15 @@ export default function RR() {
     }
 
     function add() {
-
+        setContadorID(contadorID + 1);
         if (contador <= 5) {
 
-            cq.enqueue(timer, contador);
+            cq.enqueue(timer, contadorID, prioridade);
             listaExe.push(timer);
             setContador(contador + 1);
-
+            setPrioridade(3);
+            let aux = {time:timer,id:contadorID,prio:prioridade};
+            clone.push(aux);
         }
     }
     function remove() {
@@ -172,14 +201,30 @@ export default function RR() {
         setIsOn(false);
     }
 
-
+    function compareID(a, b) {
+        if (a.identificador < b.identificador)
+            return -1;
+        if (a.identificador > b.identificador)
+            return 1;
+        return 0;
+    }
+    function zerar (){
+        setExecutando2(0);
+    }
+    function somar(){
+        setExecutando2(state=> state+1);
+    }
+    function diminuir(){
+        setExecutando2(state=>state-1);
+    }
     async function start() {
         let i = 0, aux2, cont = 0;
         acao2();
-        
+        clone.sort(compareID);
+        console.log(clone);
         somarContador(0);
         listaProntos.length = 0;
-        if (i = cq.head == 0) {
+        /*if (i = cq.head == 0) {
             for (i = cq.head; !cq.isEmpty; i++) {
                 await new Promise(resolve => setTimeout(resolve, quantum))
 
@@ -204,22 +249,61 @@ export default function RR() {
                     i = cq.head - 1;
                     acao2()
                 } else {
-                    
-                   
-                        acao()
-                    
+
+
+                    acao()
+
                 }
                 cont++;
 
-            }
+            }*/
+            zerar(0)
+            let size=clone.length;
+             for(i=0;i<=size;i++){
+                 
+                 console.log("tamanho " +size)
+                 
+                 console.log("i:"+ i);
+                  if(i==size && i!= 0){
+                    await new Promise(resolve => setTimeout(resolve, quantum))
+                    zerar()
+                      i=0;
+                      
+                  }
+                  
+                 if(clone[i].time > 0){
+                     let aux= clone[i].time - quantum;
+                     if(aux>0){
+                         clone[i].time=aux;
+                         
+                     }else{
+                       
+                         listaProntos.push(clone[i].id);
+                         clone.splice(i,1);
+                         
+                          size=clone.length;
+                         i=i-1;
+                         
+                     }
+                     
+                if(size==0){
+                    break;
+                }
+                somar();
+                
+             }
+             }
 
-        } else {
-            alert("comando inválido, para este exemplo é nessessário começar da primeira posição");
-            cq.esvaziar()
-        }
+        //} else {
+         //   alert("comando inválido, para este exemplo é nessessário começar da primeira posição");
+         //   cq.esvaziar()
+      //  }
+        cq.esvaziar()
         setContador(0);
         acao2();
         off();
+        setIsOn2(false);
+        setContadorID(0);
     }
 
     function addQuantum() {
@@ -237,7 +321,7 @@ export default function RR() {
                 <div className={style.caontainerMain}>
                     <div className={style.topBar}>
                         <div className={style.buttonVoltar}><a className='text-light' href="/"><ArrowBackIcon /></a></div>
-                        <div className={style.title + ` display-4 text-center pt-4 text-white`}>Escalonamento de CPU Preemptivo</div>
+                        <div className={style.title + ` display-4 text-center pt-4 text-white`}>Escalonamento de CPU </div>
                     </div>
                     <div className={style.part1}>
 
@@ -248,34 +332,59 @@ export default function RR() {
                             <div className={style.fila}>
                                 <div className="d-flex flex-column align-items-center p-2 text-danger">
                                     <span className="border-bottom">ID</span>
-                                    <span>Tempo  </span>
+                                    <span className="border-bottom">Tempo  </span>
+                                    <span>Prioridade  </span>
                                 </div>
-                                {vetor.map((item, i) => (<div key={i} className={(executanto == i) ? style.item + ` bg-danger` : style.item}>{(cq.list[i] != undefined) ? <div className="d-flex flex-column"> <span className="border-bottom">{cq.list[i].identificador}</span><span> {cq.list[i].time}</span> </div> : ''} </div>))}
+                                {vetor.map((item, i) => (<div key={i} className={(executanto == i) ? style.item + ` bg-danger` : style.item}>{(cq.list[i] != undefined) ? <div className="d-flex flex-column"> <span className="border-bottom">{cq.list[i].identificador}</span><span className="border-bottom"> {cq.list[i].time}</span> <span> {cq.list[i].priority}</span> </div> : ''} </div>))}
+
+                            </div>
+                            <div className={style.fila}>
+                            {vetor.map((item, i) => (<div key={i} className={(executanto2 == i) ? style.item + ` bg-danger` : style.item}>{(clone[i] != undefined) ? <div className="d-flex flex-column"> <span className="border-bottom">{clone[i].id}</span><span className="border-bottom"> {clone[i].time}</span> <span> {clone[i].prio}</span> </div> : ''} </div>))}
+
                             </div>
                         </div>
-                        <div className={style.painel2 + ` d-flex  flex-column align-items-center`}>
-                            <div className={style.boxTungle}>
-                                <h5 className={style.textPriorizar + ` text-center`} >Priorizar processos com menor tempo de execução</h5>
-                                <div className={style.switch} data-isOn={isOn} onClick={toggleSwitch}>
-                                    <motion.div className={style.handle} layout transition={spring} />
-                                </div>
-                            </div>
+                        <div className={(prioridade == 1) ? style.painel2 + `  border-danger ` : (prioridade == 2) ? style.painel2 + ` border-warning` : style.painel2 + ` border-success`}>
+
 
                             <div className={style.boxButton + ` d-flex flex-column`}>
+                                <span className="text-muted"> Criar Processo </span>
+                                <div className={style.TimerBox}>
+                                    <div className={style.spanTimer}>
+                                        <h5 className={style.texto}>Tempo de execução para cada processo: {timer} Un.t</h5>
+                                    </div>
+                                    <div className={style.addandRemove + ` d-flex justify-content-around `}>
+                                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }} className={style.buttonsInc + `  rounded-circle`} onClick={removeTimer} ><RemoveIcon /></motion.div>
+                                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className={style.buttonsInc + ` rounded-circle`} onClick={addTimer}><AddIcon /></motion.div>
 
-
-                                <div className={style.spanTimer}>
-                                    <h3 className={style.texto}>Tempo de execução para cada processo: {timer} Un.t</h3>
+                                    </div>
                                 </div>
-                                <div className={style.addandRemove + ` d-flex justify-content-around m-3 mb-5`}>
-                                    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }} className={style.buttonsInc + ` p-2 rounded-circle`} onClick={removeTimer} ><RemoveIcon /></motion.div>
-                                    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className={style.buttonsInc + ` p-2 rounded-circle`} onClick={addTimer}><AddIcon /></motion.div>
-
+                                <div className={style.prioridadeBox}>
+                                    <h5 className={style.textPriorizar + ` text-center `} >Prioridade</h5>
+                                    <div className={style.prioriButtons}>
+                                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className={style.priori1 + ` bg-success`} onClick={priority3}></motion.div>
+                                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className={style.priori1 + ` bg-warning`} onClick={priority2}></motion.div>
+                                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className={style.priori1 + ` bg-danger`} onClick={priority1}></motion.div>
+                                    </div>
                                 </div>
                                 <div className={style.buttons + ` input-group d-flex justify-content-between pb-3`}>
                                     <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} type="button" className="btn btn-danger" onClick={remove}>Remover</motion.button>
                                     <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} type="button" className="btn btn-success" onClick={add}>Adicionar</motion.button>
                                 </div>
+                                <span className="text-muted"> Executar simulação</span>
+                                <div className={style.boxTungle}>
+                                    <h5 className={style.textPriorizar + ` text-center `} >Ordenar por tempo</h5>
+                                    <div className={style.switch} data-isOn={isOn} onClick={toggleSwitch}>
+                                        <motion.div className={style.handle} layout transition={spring} />
+                                    </div>
+                                </div>
+                                <div className={style.prioriBox}>
+                                    <h5 className={style.textPriorizar + ` text-center `} >Ordenar por prioridade</h5>
+                                    <div className={style.switch2} data-isOn2={isOn2} onClick={toggleSort}>
+                                        <motion.div className={style.handle} layout transition={spring} />
+                                    </div>
+                                </div>
+
+
                                 <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} type="button" className="btn btn-success mt-2" onClick={start}>Executar</motion.button>
                             </div>
                         </div>
@@ -298,7 +407,7 @@ export default function RR() {
 
                         <div className={style.lista + ` d-flex flex-column align-items-center justify-content-center p-2`}>
                             <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }} className={style.vetorProntos}>
-                                <span className={style.spanVetor}>Processos Prontos</span>
+                                <span className={style.spanVetor}>Processos Finalizados</span>
                                 {vetor.map((item, i) => (<div key={i} className={style.cardVetor}>
                                     <div className={style.position}>{i + 1 + `°`}</div>
                                     <div className={style.item2}>{(listaProntos[i] == undefined) ? '' : `Processo ` + listaProntos[i]} </div>
