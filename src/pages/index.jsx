5ace5f5e3477c89577ca1d28002f241c3e215377
Marcos Vinicius <mvmarcos74@gmail.com>
2022-06-29 11:15:48 -0300

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,41 +1,50 @@
 import style from '../styles/Main.module.css'
 import { motion } from "framer-motion";
+import Link from 'next/link';
 import Home from "./Home";
 
 
 export default function Index() {
     return (
-       <div className={style.main}>
-            <div className= {style.container1+ ` container bg-white`}>
+        <div className={style.main}>
+            <div className={style.container1 + ` container bg-white`}>
                 <div className={style.topBar + ` d-flex justify-content-center align-items-center`}>
-                    <div className={style.title+ ` display-1 pb-2`}>Sistemas Operacionais</div>
+                    <div className={style.title + ` display-1 pb-2`}>Sistemas Operacionais</div>
                 </div>
                 <div className={style.containerMain + ` m-2`}>
                     <div className={style.titleCards + ` display-4  card text-center  shadow-sm pb-2  bg-body rounded`}>Conteúdos</div>
                     <div className={style.boxCards}>
 
 
-                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }} class={style.card}>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }} className={style.card}>
 
-                            <h5 class="card-title ">Problema Produtor-Consumidor</h5>
-                            <p class="card-text text-center ">O problema descreve dois processos, o produtor e o consumidor, que compartilham um buffer de tamanho fixo.</p>
-                            <a href='/Home' class="btn btn-primary">Acessar</a>
+                            <h5 className="card-title ">Problema Produtor-Consumidor</h5>
+                            <p className="card-text text-center ">O problema descreve dois processos, o produtor e o consumidor, que compartilham um buffer de tamanho fixo.</p>
+                            <Link href="/Home">
+                                <a className="btn btn-primary">Acessar</a>
+                            </Link>
+                            {/* <a href='/Home' className="btn btn-primary">Acessar</a> */}
 
                         </motion.div>
 
 
-                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }} class={style.card}>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }} className={style.card}>
 
-                            <h5 class="card-title  ">Escalonamento de CPU Preemptivo</h5>
-                            <p class="card-text text-center ">Escalonamento Preemptivo escolhe um processo e lhe concede a CPU durante certo tempo. Findado esse tempo, a CPU é de outro processo.</p>
-                            <a href="/RoundRobin" class="btn btn-primary">Acessar</a>
-
+                            <h5 className="card-title  ">Escalonamento de CPU Preemptivo</h5>
+                            <p className="card-text text-center ">Escalonamento Preemptivo escolhe um processo e lhe concede a CPU durante certo tempo. Findado esse tempo, a CPU é de outro processo.</p>
+                            {/* <a href="/RoundRobin" className="btn btn-primary">Acessar</a> */}
+                            <Link href="/RoundRobin">
+                                <a className="btn btn-primary">Acessar</a>
+                            </Link>
                         </motion.div >
-                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }} class={style.card}>
+                        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }} className={style.card}>
 
-                            <h5 class="card-title  ">Título do Conteúdo</h5>
-                            <p class="card-text text-center ">Breve descrição</p>
-                            <a href="#" class="btn btn-primary">Acessar</a>
+                            <h5 className="card-title  ">Título do Conteúdo</h5>
+                            <p className="card-text text-center ">Breve descrição</p>
+                            <Link href="/">
+                                <a className="btn btn-primary">Acessar</a>
+                            </Link>
+                            {/* <a href="#" className="btn btn-primary">Acessar</a> */}
 
                         </motion.div >
 
